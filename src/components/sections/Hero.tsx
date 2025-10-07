3bb6bc40992a8e873d626d303bb2f87b9a977215
afifalhauzan <afifalhauzan@student.ub.ac.id>
2025-10-07 11:06:08 +0700

    feat: add MessageCircleMore icon to consultation button in Hero section

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import TextType from '@/components/TextType';
 import Img from "next/image";
 import { motion } from "framer-motion";
+import { MessageCircleMore } from "lucide-react";
 
 export default function Hero() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -98,6 +99,7 @@ export default function Hero() {
                         text-white px-6 py-2 rounded-2xl font-semibold text-lg transition-all duration-300 hover:scale-105
                         shadow-[0_0px_35px_rgba(0,0,0,0.25)] hover:shadow-xl inset-shadow-lg inset-shadow-blue-500 shadow-purple-400/20 cursor-pointer"
                     >
+                        <MessageCircleMore />
                         <h1 className="bg-gradient-to-r from-[#F9F9FF] to-[#d4b2fb] bg-clip-text text-transparent transition-colors duration-200">Mulai Konsultasi Gratis</h1>
                     </button>
                 </div>
